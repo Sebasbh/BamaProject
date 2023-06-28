f9d01ed13bcdffc869242d50fda43030fdf66873
Nuria <np52651@gmail.com>
2023-06-28 10:27:23 +0200

    g

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import '../Styles/HomePage.css';
+import logo from '../Components/assets/Images/logo.png';
+import logoPelota from '../Components/assets/Images/logoPelota.png';
 
 function HomePage() {
   return (
-    <div>HomePage</div>
-  )
+    <div className='home'>Home
+
+        <div className='btn'>
+            <button>Clientes</button>
+            <button>Facturas</button>
+            <button>Albaranes</button>
+            <button>Pedidos</button>
+        </div>
+    </div>
+  );
 }
 
 export default HomePage
