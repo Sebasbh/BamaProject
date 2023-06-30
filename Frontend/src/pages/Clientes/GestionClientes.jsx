4edd773bbc9c8c248aabd0e49e5698a7f56cbb4f
monicadefran <monicadefran@gmail.com>
2023-06-30 14:47:06 +0200

    Adding pagination, Breadcrumb and seacrh bar

--- Frontend/src/pages/Clientes/GestionClientes.jsx
+++ Frontend/src/pages/Clientes/GestionClientes.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom' 
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.css';
+import Breadcrumb from 'react-bootstrap/Breadcrumb';
 
 
 
@@ -23,6 +24,11 @@ function GestionClientes() {
 
   }
 
+  const breadcrumbItems = [
+    { text: 'Inicio', link: '/Home' },
+    { text: 'Cliente', link: '/categoria' },
+  ];
+
   // procedimiento para eliminar un cliente
   //const deleteCliente = async (id) => {
 
@@ -33,6 +39,27 @@ function GestionClientes() {
   return (
     //<div>GestionClientes</div>
     <div className='container'>
+      <Breadcrumb>
+        {breadcrumbItems.map((item, index) => (
+          <Breadcrumb.Item
+            key={index}
+            href={item.link}
+            active={index === breadcrumbItems.length - 1}
+          >
+            {item.text}
+          </Breadcrumb.Item>
+        ))}
+      </Breadcrumb>
+      <div className="container">
+      <div className="row">
+    <div className="col-md-4">
+      <div className="input-group mb-3">
+        <input type="text" class="form-control" placeholder="Buscar..." aria-label="Buscar" aria-describedby="boton-buscar"/>
+        <button className="btn btn-primary" type="button" id="boton-buscar">Buscar</button>
+      </div>
+    </div>
+  </div>
+      </div>
       <div className='row'>
         <div className='col'> </div>
           <table className='table  table-hover'>
@@ -46,7 +73,7 @@ function GestionClientes() {
                 <th scope="col">Forma de pago</th>
               </tr>
             </thead>
-            <tbody className='table-group-divider'>
+            <tbody className='table-group-divider text-center'>
               { clientes.map ( (cliente, index) => (
                 <tr key={ index }>
                   <td> { cliente.empresa }</td>
@@ -56,12 +83,29 @@ function GestionClientes() {
                   <td> { cliente.fecha_pedido} </td>
                   <td> { cliente.forma_pago} </td>
                   <td>
-                    <Link to={`/DectalleCliente/${cliente._id}`} className='btn btn-info'> Ver Detalles </Link>
+                    <Link to={`/DectalleCliente/${cliente._id}`} className='btn btn-secondary'> Ver Detalles </Link>
                   </td>
                 </tr>
               )) }
             </tbody>
           </table>
+          <nav aria-label="Page navigation example">
+            <ul className="pagination justify-content-center">
+              <li className="page-item">
+                <a className="page-link" href="#" aria-label="Previous">
+                  <span aria-hidden="true">&laquo;</span>
+                </a>
+              </li>
+              <li className="page-item"><a class="page-link" href="#">1</a></li>
+              <li className="page-item"><a class="page-link" href="#">2</a></li>
+              <li className="page-item"><a class="page-link" href="#">3</a></li>
+              <li className="page-item">
+                <a className="page-link" href="#" aria-label="Next">
+                  <span aria-hidden="true">&raquo;</span>
+                </a>
+              </li>
+            </ul>
+          </nav>
       </div>
     </div>
   )
