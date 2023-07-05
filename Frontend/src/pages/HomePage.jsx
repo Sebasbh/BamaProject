2177296af1cadf90bd07874f39c019b55da9d7e7
Nuria <np52651@gmail.com>
2023-07-05 14:38:17 +0200

    first version client creation with bootstrap

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import '../Styles/HomePage.css';
 import logo from '../Components/assets/Images/logo.png';
-import Footer from '../Components/footer/Footer';
+import arrow from '../Components/assets/Images/arrow.png';
+import Footer from './Footer';
 
 function HomePage() {
   return (
     <div>
-        <header>
-          <img src={logo} alt='logo' className='Logo'/>
-        </header>
-        
+        <img src={logo} alt='logo' className='Logo'/>
+        <h1 className='home'>Home</h1>
         <div className='btns'>
             <button className='clientes'><h1 className='clnts'>CLIENTES<img src={arrow} className='arrow'/></h1></button>
             <button className='facturas'><h1 className='facts'>FACTURAS<img src={arrow} className='arrow'/></h1></button>
