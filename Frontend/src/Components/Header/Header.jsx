80a724c46f97087afe341959c800d5abd09bb0c3
Lucia <laylataylor28@gmail.com>
2023-07-05 23:43:29 +0200

    last version header and DetallePedidos

--- Frontend/src/Components/Header/Header.jsx
+++ Frontend/src/Components/Header/Header.jsx
@@ -1,25 +1,32 @@
 import React from 'react'
 import './Header.css'
 import logo from '../assets/Images/logo.png';
-
+import { Navbar, Nav } from 'react-bootstrap';
 
 
 function Header() {
+
   return (
     <>
-      <nav className="navbar">
-        <div className="navbar-logo">
-          <img src={logo} alt='logo' />
-        </div>
-        <ul className="navbar-menu">
-          <li className='one'>Clientes</li>
-          <li className='two'>Facturas</li>
-          <li className='three'>Pedidos</li>
-          <li className='four'>Albaranes</li>  
-        </ul>
-      </nav>
-     
+
+      <Navbar bg="white" variant="light" style={{ height: '10vh' }}>
+        <Navbar.Brand>
+          <img src={logo} alt="logo" className="navbar-logo" style={{ height: '15vh', marginRight: 'auto', padding: '30px' }} />
+        </Navbar.Brand>
+        <Nav className="ml-auto" style={{ marginLeft: 'auto', padding: '30px' }}>
+          <Nav.Link className="one">Clientes</Nav.Link>
+          <Nav.Link className="two">Facturas</Nav.Link>
+          <Nav.Link className="three">Pedidos</Nav.Link>
+          <Nav.Link className="four">Albaranes</Nav.Link>
+        </Nav>
+      </Navbar>
+
+
+
+
+
     </>
+
   )
 }
 
