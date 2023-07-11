3530f91b4ab8b2bcb970dce690764154ae048f8e
Nuria <np52651@gmail.com>
2023-07-11 10:17:06 +0200

    final home

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import '../Styles/HomePage.css';
 import logo from '../Components/assets/Images/logo.png';
-import arrow from '../Components/assets/Images/arrow.png';
 import Footer from '../Components/footer/Footer';
 
 function HomePage() {
@@ -10,13 +9,12 @@ function HomePage() {
         <header>
           <img src={logo} alt='logo' className='Logo'/>
         </header>
-        
-        <h1 className='home'>Home</h1>
+        <h1 className='title'>Home</h1>
         <div className='btns'>
-            <button className='clientes'><h1>CLIENTES<img src={arrow} className='arrow'/></h1></button>
-            <button className='facturas'><h1>FACTURAS<img src={arrow} className='arrow'/></h1></button>
-            <button className='albaranes'><h1>ALBARANES<img src={arrow} className='arrow'/></h1></button>
-            <button className='pedidos'><h1>PEDIDOS<img src={arrow} className='arrow'/></h1></button>
+            <button className='clientes'><h1>CLIENTES <h2>^</h2></h1></button>
+            <button className='facturas'><h1>FACTURAS <h2>^</h2></h1></button>
+            <button className='albaranes'><h1>ALBARANES <h2>^</h2></h1></button>
+            <button className='pedidos'><h1>PEDIDOS <h2>^</h2></h1></button>
         </div>
         <Footer />
     </div>
