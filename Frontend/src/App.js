01f7ccbfa7cd13998cd67049ceabce7c4eda0a9c
monicadefran <monicadefran@gmail.com>
2023-07-13 09:26:48 +0200

    Routes fixed

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -4,10 +4,11 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
-import CDcliente from './pages/Clientes/CDcliente';
-import DetalleCliente from './pages/Clientes/DetalleCliente';
+import DetalleCliente from './pages/Clientes/DetalleCliente'
+import FormularioClientes from './pages/Clientes/FormularioClientes';
 import NotFound from './pages/NotFound';
 import GestionClientes from './pages/Clientes/GestionClientes';
+import DetalleFactura from './pages/Facturas/DetalleFactura';
 
 const App = () => {
   return (
@@ -16,9 +17,10 @@ const App = () => {
       
         <Route path="/Home" element={<HomePage />} />
         <Route path="/" element={<LoginPage/>} />
-        <Route path="/cdcliente" element={<CDcliente/>} />
-        <Route path="/DectalleCliente" element={<DetalleCliente/>} />
         <Route path="/GestionClientes" element={<GestionClientes/>}/>
+        <Route path="/DetalleFactura" element={<DetalleFactura/>} />
+        <Route path="/DetalleCliente/:id" element={<DetalleCliente/>} />
+        <Route path="/FormularioClientes" element={<FormularioClientes />} />
         <Route element={NotFound} />
        </Routes>
     </Router>
