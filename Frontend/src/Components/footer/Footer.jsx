3352a15829c891cb04d9883b0ef6b0e09b9b4c65
Nuria <np52651@gmail.com>
2023-07-14 11:45:46 +0200

    footer con bootstrap

--- Frontend/src/Components/footer/Footer.jsx
+++ Frontend/src/Components/footer/Footer.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
-import '../../Styles/footer.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function Footer() {
-    return (
-      <div className='footer'> 
-        <h1 className='frase-footer' >
-          "Generamos la ventaja competitiva que necesitas."
-        </h1>
-      </div>
-    );
-  }
+  return (
+    <div className="footer">
+      <h1 className="frase-footer text-center text-white">
+        "Generamos la ventaja competitiva que necesitas."</h1>
+    </div>
+  );
+}
 
-  export default Footer;
+export default Footer;
