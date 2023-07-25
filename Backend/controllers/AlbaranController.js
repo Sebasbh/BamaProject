d93b96b497c60026b5904c523bd46c432fc3a416
monicadefran <monicadefran@gmail.com>
2023-07-25 14:24:46 +0200

    Adding the upload file opcion to Albaranes

--- Backend/controllers/AlbaranController.js
+++ Backend/controllers/AlbaranController.js
@@ -1,4 +1,34 @@
 import { Albaran } from '../models/AllModels.js';
+import multer from 'multer';
+import path from 'path';
+import express from 'express';
+
+
+//Guardar con storage el archivo
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/'); // Lo guarda en la carpeta uploads
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  // Accept only specific file types, if needed (e.g., accept only images)
+  const allowedFileTypes = /jpeg|jpg|png|gif|pdf/;
+  const extname = allowedFileTypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = allowedFileTypes.test(file.mimetype);
+  if (extname && mimetype) {
+    return cb(null, true);
+  } else {
+    cb(new Error('Only images are allowed!'));
+  }
+};
+
+const upload = multer({ storage, fileFilter });
+
 
 // Mostrar todos los Albaranes
 const getAllAlbaranes = async (req, res) => {
@@ -26,19 +56,31 @@ const getAlbaran = async (req, res) => {
 
 // Crear un albaran
 const createAlbaran = async (req, res) => {
-  try {
-     const ultimoAlbaran = await Albaran.findOne().sort({ numero_de_albaran: -1 }).exec();
-     const numeroAlbaran = ultimoAlbaran ? ultimoAlbaran.numero_de_albaran + 1 : 1;
-     const albaran = await Albaran.create({ ...req.body, numero_de_albaran: numeroAlbaran });
-     
-     res.status(200).json({
-        message: "¡Albaran creado correctamente!", albaran
-     });
-  } catch (error) {
-     res.json({ message: error.message });
-   }
+  // The multer middleware should be placed inside the function, not before it
+  upload.single('archivo')(req, res, async (err) => {
+    if (err instanceof multer.MulterError) {
+      // Handle multer errors, if any
+      return res.status(400).json({ message: 'Error uploading the file.' });
+    } else if (err) {
+      // Handle other errors, if any
+      return res.status(500).json({ message: 'Internal server error.' });
+    }
 
+    try {
+      const ultimoAlbaran = await Albaran.findOne().sort({ numero_de_albaran: -1 }).exec();
+      const numeroAlbaran = ultimoAlbaran ? ultimoAlbaran.numero_de_albaran + 1 : 1;
+      const albaran = await Albaran.create({ ...req.body, numero_de_albaran: numeroAlbaran });
+
+      res.status(200).json({
+        message: '¡Albaran creado correctamente!',
+        albaran,
+      });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  });
 };
+
 
 // Actualizar un Albaran
 const updateAlbaran = async (req, res) => {
--- Frontend/src/pages/Albaranes/FormularioAlbaranes.jsx
+++ Frontend/src/pages/Albaranes/FormularioAlbaranes.jsx
@@ -12,12 +12,16 @@ function FormularioAlbaranes() {
   const [pedido_id, setPedidoId] = useState('');
   const [importe, setImporte] = useState('');
   const [estado, setEstado] = useState('');
+  const [file, setFile] = useState(null);
   const [albaranAgregado, setAlbaranAgregado] = useState(false); // Nuevo estado para el mensaje de albaran agregado
   const navigate = useNavigate();
 
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    const formData = new FormData();
+    formData.append('archivo', file); // Añade el archivo
 
     const albaran = {
       numero_de_albaran,
@@ -29,17 +33,37 @@ function FormularioAlbaranes() {
     };
 
     try {
-      const response = await axios.post('http://localhost:8000/albaranes', albaran);
+      const response = await axios.post(
+        "http://localhost:8000/albaranes",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data", // Make sure to set the correct content type for file uploads
+          },
+          params: albaran, // Include other data as URL params
+        }
+      );
       if (response.status === 201) {
-        setAlbaranAgregado(true); // Actualizar el estado de albaranAgregado cuando se completa la solicitud
-        alert('Albarán creado correctamente.');
-        navigate('/GestionAlbaranes');
+        setAlbaranAgregado(true);
+        alert("Albarán creado correctamente.");
+        navigate("/GestionAlbaranes");
       } else {
-        alert('Error al crear el albarán.');
+        alert("Error al crear el albarán.");
       }
     } catch (error) {
       console.log(error);
-      alert('Error al crear el albarán.');
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        alert('Error al cargar el archivo. Inténtalo de nuevo.');
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in Node.js
+        alert('Sin respuesta del servidor. Por favor revise su conexion a internet.');
+      } else {
+        alert('Ocurrió un error al procesar la solicitud. Por favor, inténtelo de nuevo más tarde.');
+      }
     }
   };
 
@@ -63,7 +87,7 @@ function FormularioAlbaranes() {
                     value={numero_de_albaran}
                     onChange={(e) => setNumeroDeAlbaran(e.target.value)}
                     required
-                    style={{ width: '500px', height: '40px' }}
+                    style={{ width: "500px", height: "40px" }}
                   />
                 </Form.Group>
               </Col>
@@ -74,7 +98,7 @@ function FormularioAlbaranes() {
                     type="text"
                     value={cliente_id}
                     onChange={(e) => setClienteId(e.target.value)}
-                    style={{ width: '500px', height: '40px' }}
+                    style={{ width: "500px", height: "40px" }}
                   />
                 </Form.Group>
               </Col>
@@ -88,7 +112,7 @@ function FormularioAlbaranes() {
                     value={fecha_albaran}
                     onChange={(e) => setFechaAlbaran(e.target.value)}
                     required
-                    style={{ width: '500px', height: '40px' }}
+                    style={{ width: "500px", height: "40px" }}
                   />
                 </Form.Group>
               </Col>
@@ -100,7 +124,7 @@ function FormularioAlbaranes() {
                     value={importe}
                     onChange={(e) => setImporte(e.target.value)}
                     required
-                    style={{ width: '500px', height: '40px' }}
+                    style={{ width: "500px", height: "40px" }}
                   />
                 </Form.Group>
               </Col>
@@ -113,7 +137,7 @@ function FormularioAlbaranes() {
                     value={estado}
                     onChange={(e) => setEstado(e.target.value)}
                     required
-                    style={{ width: '500px', height: '40px' }}
+                    style={{ width: "500px", height: "40px" }}
                   >
                     <option value="">Seleccione una opción</option>
                     <option value="Firmado">Firmado</option>
@@ -129,10 +153,22 @@ function FormularioAlbaranes() {
                     value={pedido_id}
                     onChange={(e) => setPedidoId(e.target.value)}
                     required
-                    style={{ width: '500px', height: '40px' }}
+                    style={{ width: "500px", height: "40px" }}
                   />
                 </Form.Group>
               </Col>
+              <Row className="mb-3">
+              <Col md={12}>
+                <Form.Group controlId="adjuntarArchivo">
+                  <Form.Label>Adjuntar Archivo</Form.Label>
+                  <Form.Control
+                    type="file"
+                    onChange={(e) => setFile(e.target.files[0])}
+                    style={{ width: '500px' }}
+                 />
+                </Form.Group>
+              </Col>
+              </Row>
             </Row>
             <Button variant="primary" type="submit" className="mt-3">
               Crear albaran
