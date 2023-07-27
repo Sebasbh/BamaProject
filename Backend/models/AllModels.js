9f89e167ab16485a93efbe57ff5455edd720503e
monicadefran <monicadefran@gmail.com>
2023-07-27 19:24:21 +0200

    Working in the Facturas formulario

--- Backend/models/AllModels.js
+++ Backend/models/AllModels.js
@@ -38,19 +38,16 @@ const AlbaranSchema = new mongoose.Schema({
 const FacturaSchema = new mongoose.Schema({
   numero_de_factura: { type: Number, /* required: true, */ unique: true },
   empresa: { type: String, /* required: true, */ },
-  //fecha_de_factura: { type: Date, default: Date.now },
-  fecha_de_factura: { type: String, /* required: true, */ },
+  fecha_de_factura: { type: Date, default: Date.now },
   vencimiento: { type: String, enum: ['Al contado', '30 días fecha factura', '60 días fecha factura'], /* required: true */ },
   base_imponible: { type: Number, /* required: true */ },
   tipo_de_IVA: { type: String, enum: ['21%', '10%', '4%', 'Sin IVA'], /* required: true*/ },
   importe_IVA: { type: Number },
   total_factura: { type: Number },
   estado_factura: { type: String, enum: ['En trámite', 'Cerrada'], default: 'En trámite' },
-  //fecha_de_cobro: { type: Date },
-  fecha_de_cobro: { type: String, /* required: true, */ },
-  pedido_id: { type: mongoose.Schema.Types.ObjectId, ref: 'pedidos', /* required: true */ },
-  albaran_id: { type: mongoose.Schema.Types.ObjectId, ref: 'albaranes' },
-  archivo_de_factura: { type: String }
+  fecha_de_cobro: { type: Date, default: Date.now },
+  numero_de_pedido: { type: Number, /* required: true */ },
+  numero_de_albaran: { type: Number, /* required: true */ }
 });
 
 const Cliente = mongoose.model('clientes', ClienteSchema);
--- Frontend/src/pages/Facturas/CrearFactura.jsx
+++ Frontend/src/pages/Facturas/CrearFactura.jsx
@@ -1,69 +1,147 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Container, Form, Button, Alert } from 'react-bootstrap';
+import { Container, Form, Button, Alert, FormGroup, Row, Col } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
-import Footer from '../../Components/footer/Footer';
+import Header from '../../Components/Header/Header';
+
+
+const api = axios.create({
+  baseURL: 'http://localhost:8000'
+});
+
+const fetchClientesPedidosAlbaranFacturaNumber = async () => {
+  try {
+    const response = await Promise.all([
+      api.get('/clientes'),
+      api.get('/pedidos'),
+      api.get('/albaranes'),
+      api.get('/facturas/next/number')
+    ]);
+
+    const clientes = response[0].data;
+    const pedidos = response[1].data;
+    const albaran = response[2].data;
+    const nextFacturaNumber = response[3].data.nextFacturaNumber;
+
+    return { clientes, pedidos, albaran, nextFacturaNumber };
+  } catch (error) {
+    throw new Error('Error al cargar los datos.');
+  }
+};
 
 function CrearFactura() {
-    const [numeroDeFactura, setNumeroDeFactura] = useState('');
-  const [fechaDeFactura, setFechaDeFactura] = useState('');
+  const [numeroDeFactura, setNumeroDeFactura] = useState('');
   const [cliente, setCliente] = useState('');
-  const [importe, setImporte] = useState('');
+  const [fechaDeFactura, setFechaDeFactura] = useState('');
+  const [vencimiento, setVencimiento] = useState('');
+  const [importeIVA, setImporteIVA] = useState('');
+  const [baseImponible, setBaseImponible] = useState('');
+  const [tipoDeIVA, setTipoDeIVA] = useState('');
+  const [totalFactura, setTotalFactura] = useState('');
+  const [estadoFactura, setEstadoFactura] = useState('');
+  const [fechaDeCobro, setFechadeCobro] = useState('');
+  const [pedido, setPedido] = useState('');
+  const [albaran, setAlbaran] = useState('');
   const [archivoAdjunto, setArchivoAdjunto] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState('');
+  const [message, setMessage] = useState('');
 
-  const navigate = useNavigate();
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const { clientes, pedidos, albaran, nextFacturaNumber } = await fetchClientesPedidosAlbaranFacturaNumber();
+        setCliente(clientes); 
+        setPedido(pedidos);
+        setAlbaran(albaran);
+        setFechaDeFactura(nextFacturaNumber);
+        setError(null);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
 
-  const handleSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault();
 
-    // Validar campos requeridos antes de enviar el formulario
-    if (!numeroDeFactura || !fechaDeFactura || !cliente || !importe) {
-      setError('Por favor, complete todos los campos obligatorios.');
+    if (!cliente || !fechaDeFactura || !vencimiento || !baseImponible || !tipoDeIVA || !importeIVA || !totalFactura || !estadoFactura || !fechaDeCobro || !pedido || !albaran) {
+      setError('Por favor, rellena todos los campos.');
       return;
     }
 
-    setError('');
+   
+    const isApproved = true; //
+
+
+    const newFactura = {
+      numero_de_factura: numeroDeFactura,
+      cliente: cliente,
+      fecha_de_factura: new Date(fechaDeFactura), // Convert the fecha to a Date object
+      vencimiento: vencimiento,
+      base_imponible: baseImponible,
+      tipo_de_IVA: tipoDeIVA,
+      importe_IVA: importeIVA,
+      total_factura: totalFactura,
+      estado_factura: estadoFactura,
+      fecha_de_cobro: new Date(fechaDeCobro), 
+      archivo_de_adjunto: '',
+      estado: estadoFactura,
+      isApproved: isApproved, 
+    };
+
     setIsLoading(true);
-
-    const factura = new FormData();
-    factura.append('numero_de_pedido', numeroDeFactura);
-    factura.append('fecha_de_pedido', fechaDeFactura);
-    factura.append('cliente', cliente);
-    factura.append('importe', importe);
-    factura.append('archivo_adjunto', archivoAdjunto);
-
-    axios
-      .post('', factura)
-      .then((response) => {
-        console.log(response.data);
-        navigate('/GestionFactura');
-      })
-      .catch((error) => {
-        console.log(error);
-        setError('Ocurrió un error al crear la factura.');
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
+    try {
+      const { data } = await api.post('/facturas', newFactura);
+      setMessage('¡Factura creada correctamente!');
+      
+      setNumeroDeFactura('');
+      setCliente('');
+      setFechaDeFactura('');
+      setVencimiento('');
+      setImporteIVA('');
+      setBaseImponible('');
+      setTipoDeIVA('');
+      setTotalFactura('');
+      setEstadoFactura('');
+      setFechadeCobro('');
+      setPedido('');
+      setAlbaran('');
+      setArchivoAdjunto(null);
+      setError(null);
+    } catch (error) {
+      setError('Error al crear la factura. Por favor, inténtelo nuevamente.');
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   return (
-    <div>
+    <Container>
+    <Header/>
+    <div style={{ marginTop: '20px', marginBottom: '50px' }}>
     <Container className='justify-content-center mt-5'>
       <h3>Registrar Factura</h3>
       {error && <Alert variant="danger">{error}</Alert>}
-      <Form onSubmit={handleSubmit}>
+      <Form onSubmit={onSubmit}>
+
+        <Row>
+        <Col sm={12} md={6}>
         <Form.Group controlId="numero_de_factura">
           <Form.Label className='mt-4'>Número de Factura:</Form.Label>
           <Form.Control
             type="number"
-            onChange={(e) => setNumeroDeFactura(e.target.value)}
-            required
+            value={numeroDeFactura} 
+            readOnly
           />
         </Form.Group>
+        </Col> 
 
+        <Col sm={12} md={6}>
         <Form.Group controlId="fecha_de_factura">
           <Form.Label className='mt-4'>Fecha de Factura:</Form.Label>
           <Form.Control
@@ -72,7 +150,20 @@ function CrearFactura() {
             required
           />
         </Form.Group>
+        </Col> 
 
+        <Col sm={12} md={6}>
+        <Form.Group controlId="fecha_de_cobro">
+          <Form.Label className='mt-4'>Fecha de cobro:</Form.Label>
+          <Form.Control
+            type="date"
+            onChange={(e) => setFechadeCobro(e.target.value)}
+            required
+          />
+        </Form.Group>
+        </Col> 
+
+        <Col sm={12} md={6}>
         <Form.Group controlId="cliente">
           <Form.Label className='mt-4'>Cliente:</Form.Label>
           <Form.Control
@@ -81,16 +172,51 @@ function CrearFactura() {
             required
           />
         </Form.Group>
+        </Col> 
 
-        <Form.Group controlId="importe">
-          <Form.Label className='mt-4'>Importe:</Form.Label>
+        <Col sm={12} md={6}>
+        <Form.Group>
+          <Form.Label className='mt-4'>Tipo de IVA:</Form.Label>
+          <Form.Select
+          type='select'              
+          value={tipoDeIVA}
+          onChange={(e) => setTipoDeIVA(e.target.value)}
+          required
+          >
+          <option value="">Seleccione una opción</option>
+          <option value="21%">21%</option>
+          <option value="10%">10%</option>
+          <option value="4%">4%</option>
+          <option value="sin IVA">Sin IVA</option>
+          </Form.Select>
+        </Form.Group>
+        </Col>
+
+        <Col sm={12} md={6}>
+        <Form.Group controlId="importe_IVA">
+          <Form.Label className='mt-4'>Importe IVA:</Form.Label>
           <Form.Control
             type="number"
-            onChange={(e) => setImporte(e.target.value)}
+            onChange={(e) => setImporteIVA(e.target.value)}
             required
           />
         </Form.Group>
+        </Col>
+        </Row>
 
+        <Row>
+        <Col sm={12} md={6}>
+        <Form.Group controlId="base_imponible">
+          <Form.Label className='mt-4'>Base Imponible:</Form.Label>
+          <Form.Control
+            type="number"
+            onChange={(e) => setBaseImponible(e.target.value)}
+            required
+          />
+        </Form.Group>
+        </Col>
+
+        <Col sm={12} md={6}>
         <Form.Group>
           <Form.Label className='mt-4'>Adjuntar Archivo:</Form.Label>
           <Form.Control
@@ -98,16 +224,62 @@ function CrearFactura() {
             onChange={(e) => setArchivoAdjunto(e.target.files[0])}
           />
         </Form.Group>
+        </Col>
+
+        <Col sm={12} md={6}>
+        <Form.Group>
+          <Form.Label className='mt-4'>Vencimiento:</Form.Label>
+          <Form.Select
+          type='select'              
+          value={vencimiento}
+          onChange={(e) => setVencimiento(e.target.value)}
+          required
+          >
+          <option value="">Seleccione una opción</option>
+          <option value="Al contado">Al contado</option>
+          <option value="30 dias">30 Días</option>
+          <option value="60 dias">60 Días</option>
+          </Form.Select>
+        </Form.Group>
+        </Col>
+
+        <Col sm={12} md={6}>
+        <Form.Group>
+          <Form.Label className='mt-4'>Estado factura:</Form.Label>
+          <Form.Select
+          type='select'              
+          value={estadoFactura}
+          onChange={(e) => setEstadoFactura(e.target.value)}
+          required
+          >
+          <option value="">Seleccione una opción</option>
+          <option value="en tramite">En tramite</option>
+          <option value="cerrada">Cerrada</option>
+          </Form.Select>
+        </Form.Group>
+        </Col>
+
+        <Col sm={12} md={6}>
+        <Form.Group controlId="total_factura">
+          <Form.Label className='mt-4'>Total factura:</Form.Label>
+          <Form.Control
+            type="number"
+            onChange={(e) => setTotalFactura(e.target.value)}
+            required
+          />
+        </Form.Group>
+        </Col>
+        </Row>
 
         <Button className='mt-5' variant="primary" type="submit" disabled={isLoading}>
-          {isLoading ? 'Cargando...' : 'Registrar Factura'}
-        </Button>
-      </Form> 
+              {isLoading ? 'Cargando...' : 'Registrar Factura'}
+            </Button>
+          </Form>
+        </Container>
+      </div>
     </Container>
-    <Footer />
-    </div>
   );
-};
+}
 
 
-export default CrearFactura
+export default CrearFactura;
